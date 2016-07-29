041596b7a3f049fb6ba6afae2b8d03abe42e7ac4
Yanru <yrwang.ari@gmail.com>
2016-07-29 00:55:29 -0500

    tenfold_test_fri_0055

---
+++ tenfold_test.js
@@ -0,0 +1,7 @@
+function popup_show(){
+    document.getElementById('tfd_popup').style.display = 'block';
+}
+
+function popup_hide(){
+    document.getElementById('tfd_popup').style.display = 'none';
+}
